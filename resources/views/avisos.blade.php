ee3e2a99e3ed820ee1335bb9c2c2d967dad4435f
MATHEUS MAKOTO KODAMA <matheus.mkodama@senacsp.edu.br>
2022-03-11 20:08:36 -0300

    inicio

---
+++ resources/views/avisos.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.externo');
+@section('title','minha primeira view')
+@section('sidebar')
+
+    @parent
+    <hr>
+    @endsection
+    @section('content')
+    <table class="table">
+        
+    </table>
