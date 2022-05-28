670fb98eab123eeac827897ad460644f46eca09c
MATHEUS MAKOTO KODAMA <matheus.mkodama@senacsp.edu.br>
2022-05-27 22:22:04 -0300

    x

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -13,7 +13,63 @@
         </div>
     </div>
 </div>
+<br>
+
+
+@if (count($errors) > 0)
+
+  <div class="alert alert-danger">
+    <strong>Ops!</strong> Há algo errado com os dados passados.<br><br>
+    <ul>
+       @foreach ($errors->all() as $error)
+
+         <li>{{ $error }}</li>
+
+       @endforeach
+    </ul>
+  </div>
+
+@endif
+
+
+            <strong>Nome:</strong>
+
+            {!! Form::text('nome', null, array('placeholder' => 'Nome','class' => 'form-control')) !!}
+
+        </div>
+    </div>
 </br>
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Endereço:</strong>
+            {!! Form::text('endereco', null, array('placeholder' => 'Endereço','class' => 'form-control')) !!}
+        </div>
+    </div>
+
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Telefone:</strong>
+
+
+@extends('layouts.app')
+
+@section('content')
+
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Cadastrar cliente</h2>
+        </div>
+
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('clients.index') }}"> Voltar</a>
+        </div>
+    </div>
+</div>
+<br>
 
 
 @if (count($errors) > 0)
@@ -69,7 +125,7 @@
     </div>
 
 </div>
+@if (count($errors) > 0)
 
-{!! Form::close() !!}
-
-@endsection
+  <div class="alert alert-danger">
+    <strong>Ops!</strong> Há algo errado com os dados passados.<br><br>
